6c28ac0eec5c6bcf2f769bda2a973117c48cf96a
enriquesalgadojuarezcb-op <enriquesalgadojuarez.cb@gmail.com>
2026-03-23 11:12:26 -0600

    Boton de bebidas completo

---
+++ AgregarBebida.xaml.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Riso
+{
+    public partial class AgregarBebida : Window
+    {
+        string cadenaConexion = "Server=localhost; Database=RisoRestaurant; Uid=root; Pwd=;";
+
+        public AgregarBebida()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    string consulta = "INSERT INTO Bebidas (Nombre, Costo) VALUES (@nombre, @costo)";
+                    MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                    comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                    comando.Parameters.AddWithValue("@costo", txtPrecio.Text);
+                    comando.ExecuteNonQuery();
+
+                    MessageBox.Show("¡Bebida agregada con éxito!");
+                    txtNombre.Clear();
+                    txtPrecio.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            Bebidas ventanaBebidas = new Bebidas();
+            ventanaBebidas.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Bebidas.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Riso
+{
+    public partial class Bebidas : Window
+    {
+        // La ruta para llegar a tu base de datos de XAMPP
+        string cadenaConexion = "Server=localhost; Database=RisoRestaurant; Uid=root; Pwd=;";
+
+        public Bebidas()
+        {
+            InitializeComponent();
+            CargarDatosTabla(); // Esto llena la tabla en cuanto se abre la ventana
+        }
+
+        // Esta es la función que te marcaba error porque no existía
+        private void CargarDatosTabla()
+        {
+            try
+            {
+                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    // Seleccionamos las columnas exactas de tu tabla Bebidas
+                    string consulta = "SELECT idBebidas, Nombre, Costo FROM Bebidas";
+                    MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+
+                    // Asigna los datos al DataGrid de tu diseño
+                    dgBebidas.ItemsSource = tabla.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+            }
+        }
+
+        private void btnAgregarBebida_Click(object sender, RoutedEventArgs e)
+        {
+            AgregarBebida ventana = new AgregarBebida();
+            ventana.Show();
+            this.Close();
+        }
+
+        private void btnEliminarBebida_Click(object sender, RoutedEventArgs e)
+        {
+            EliminarBebida ventana = new EliminarBebida();
+            ventana.Show();
+            this.Close();
+        }
+
+        private void btnActualizar_Click(object sender, RoutedEventArgs e)
+        {
+            CargarDatosTabla(); // Ahora sí existe y funcionará el botón
+            MessageBox.Show("Tabla actualizada");
+        }
+
+        private void btnVolver_Click(object sender, RoutedEventArgs e)
+        {
+            // OJO: Si tu ventana principal se llama MenuPrincipal, cámbialo aquí
+            MainWindow menu = new MainWindow();
+            menu.Show();
+            this.Close();
+        }
+    }
+}
